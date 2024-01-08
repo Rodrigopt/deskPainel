ee827f2d4d9094d3bef35a368d97240bbb8bcdc4
Rodrigopt <rodrigopte23@gmail.com>
2024-01-08 15:54:28 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,62 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card, CardHeader, CardBody, CardText, CardTitle} from 'reactstrap';
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
 function App() {
   return (
@@ -13,7 +69,7 @@ function App() {
       width: '18rem'
     }}
   >
-    <CardHeader tag="h5">
+    <CardHeader tag="h5" >
       MTTA
     </CardHeader>
     <CardBody>
@@ -105,6 +161,7 @@ function App() {
       </CardText>
     </CardBody>
   </Card>
+  <Line options={options} data={data} />
 </>
   );
 }
